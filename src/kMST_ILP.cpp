7e7c054766c5c8d25896c8a77738b09c9dd5cec4
Jakob Gruber <jakob.gruber@gmail.com>
2014-01-05 15:07:12 +0100

    The artificial root transmits k goods
    One per node in the spanning tree.

--- src/kMST_ILP.cpp
+++ src/kMST_ILP.cpp
@@ -198,9 +198,9 @@ Variables *kMST_ILP::modelSCF()
 	model.add(k == e_num_nodes);
 	e_num_nodes.end();
 
-	/* $\sum_j f_{0j} = k - 1$. The artificial root sends k - 1 goods.
+	/* $\sum_j f_{0j} = k$. The artificial root sends k goods.
 	 * $\forall i: f_{i0} = 0$. No goods are transmitted to the artificial root.
-	 * $\forall i, j: f_{ij} \leq (k - 1)x_{ij}$. Only active edges transport goods. */
+	 * $\forall i, j: f_{ij} \leq kx_{ij}$. Only active edges transport goods. */
 
 	IloExpr e_aroot_tx(env);
 	for (u_int k = 0; k < n_edges; k++) {
@@ -216,10 +216,10 @@ Variables *kMST_ILP::modelSCF()
 		}
 
 		IloExpr e_active_goods(env);
-		e_active_goods = v->fs[k] - v->xs[k] * (int)(k - 1);
+		e_active_goods = v->fs[k] - v->xs[k] * (int)k;
 		model.add(e_active_goods <= 0);
 	}
-	model.add(e_aroot_tx == k - 1);
+	model.add(e_aroot_tx == k);
 
 	/* $\forall j: \sum_i f_{ij} - \sum_k f_{jk} = v_i$.
 	 * Each active node consumes one unit. */
